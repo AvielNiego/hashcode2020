ce8e8e78fe379743ddeca6835e25082ef58b6c7b
yiz <yizhar.gilboa@planckresolution.com>
2020-02-20 20:56:37 +0200

    WIP - naive scorer

--- src/com/hashcode/ProblemSolver.java
+++ src/com/hashcode/ProblemSolver.java
@@ -1,9 +1,19 @@
 package com.hashcode;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ProblemSolver {
+
+    private List<Integer> books;
     public List<LibrarySubmission> solve(InputData inputData) {
+        books = inputData.books;
+        inputData.libraries.sort(Comparator.comparingInt(this::getLibraryScore).reversed());
+//        inputData.libraries.stream().map(l -> new LibrarySubmission(l.));
         return null;
+    }
+
+    private int getLibraryScore(Library lib1) {
+        return lib1.getBooks().stream().mapToInt(x -> books.get(x)).sum();
     }
 }
