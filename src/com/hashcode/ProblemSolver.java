18703a19a0221b53a27d42ec9e2bd9b3c708b4fb
yiz <yizhar.gilboa@planckresolution.com>
2020-02-20 21:18:09 +0200

    Merge branch 'master' of https://github.com/AvielNiego/hashcode2020
    # Conflicts:
    #	src/com/hashcode/Main.java
    #	src/com/hashcode/ScoreCalculator.java

--- src/com/hashcode/ProblemSolver.java
+++ src/com/hashcode/ProblemSolver.java
@@ -2,15 +2,30 @@ package com.hashcode;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.lang.System.currentTimeMillis;
 
 public class ProblemSolver {
 
     private List<Integer> books;
+    private Map<Integer, Integer> libraryScore;
+
     public List<LibrarySubmission> solve(InputData inputData) {
         books = inputData.books;
+
+
+        inputData.libraries.forEach(lib -> libraryScore.put(lib.getLibraryIndex(), getLibraryScore(lib)));
         inputData.libraries.sort(Comparator.comparingInt(this::getLibraryScore).reversed());
-//        inputData.libraries.stream().map(l -> new LibrarySubmission(l.));
-        return null;
+
+        long s = currentTimeMillis();
+        inputData.libraries.forEach(l -> books.sort(Comparator.comparingInt(o -> books.get(o))));
+        long t = currentTimeMillis();
+        System.out.println(t - s);
+
+
+        return inputData.libraries.stream().map(l -> new LibrarySubmission(l.getLibraryIndex(), l.books)).collect(Collectors.toList());
     }
 
     private int getLibraryScore(Library lib1) {
