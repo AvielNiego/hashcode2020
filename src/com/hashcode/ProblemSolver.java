ec4ee7ff1a68b17657da819f50fa77aa7b27ce77
yiz <yizhar.gilboa@planckresolution.com>
2020-02-20 22:05:33 +0200

    added set of remaining books up to end date per lib

--- src/com/hashcode/ProblemSolver.java
+++ src/com/hashcode/ProblemSolver.java
@@ -1,6 +1,8 @@
 package com.hashcode;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -10,18 +12,23 @@ public class ProblemSolver {
     private Map<Integer, Integer> libraryScore;
     private InputData inputData;
     private Integer remainingDays;
+    private Set<Integer> chosenBooks;
 
     public List<LibrarySubmission> solve(InputData inputData) {
         libraryScore = new HashMap<>();
         this.inputData = inputData;
         remainingDays = this.inputData.getDaysForScanning();
         books = inputData.books;
+        chosenBooks = new HashSet<>();
 
 
         List<LibrarySubmission> chosenLibs = new ArrayList<>();
         while (remainingDays > 0 && !inputData.libraries.isEmpty()) {
             Library chosenLib = getNextLibraryBySortingMethod(inputData.libraries);
-            chosenLibs.add(new LibrarySubmission(chosenLib.getLibraryIndex(), chosenLib.booksIndex));
+            chosenBooks.addAll(chosenLib.getBooksIndex());
+            List<Integer> booksUpToRemainingTimeFromLib = getBooksUpToRemainingTime(chosenLib).collect(Collectors.toList());
+            chosenBooks.addAll(booksUpToRemainingTimeFromLib);
+            chosenLibs.add(new LibrarySubmission(chosenLib.getLibraryIndex(), booksUpToRemainingTimeFromLib));
             remainingDays -= chosenLib.getSignUpTime();
         }
 
@@ -30,14 +37,10 @@ public class ProblemSolver {
     }
 
     private Library getNextLibraryBySortingMethod(List<Library> libraries) {
-        long s = currentTimeMillis();
         libraries.forEach(l -> l.booksIndex.sort(Comparator.comparingInt(o -> books.get(o))));
-        long t = currentTimeMillis();
         libraries.forEach(lib -> libraryScore.put(lib.getLibraryIndex(), getLibraryScore(lib)));
         libraries.sort(Comparator.comparingInt(this::queryScore).reversed());
-        Library chosenLib = libraries.remove(0);
-        System.out.println(t - s);
-        return chosenLib;
+        return libraries.remove(0);
     }
 
     private int queryScore(Library lib) {
@@ -46,10 +49,16 @@ public class ProblemSolver {
 
 
     private int getLibraryScore(Library lib1) {
-        long daysRemaining = Math.max(inputData.getDaysForScanning() - lib1.getSignUpTime(), 0);
-        return lib1.getBooksIndex().stream()
-                .limit(daysRemaining * lib1.getShipsPerDay())
+        Stream<Integer> booksUpToRemainingTime = getBooksUpToRemainingTime(lib1);
+        return booksUpToRemainingTime
                 .mapToInt(x -> books.get(x))
                 .sum();
+    }
+
+    private Stream<Integer> getBooksUpToRemainingTime(Library lib1) {
+        long daysRemaining = Math.max(inputData.getDaysForScanning() - lib1.getSignUpTime(), 0);
+        return lib1.getBooksIndex().stream()
+                .filter(bookidx -> !chosenBooks.contains(bookidx))
+                .limit(daysRemaining * lib1.getShipsPerDay());
     }
 }
