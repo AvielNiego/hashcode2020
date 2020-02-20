7c62e518bf6eae146828ba30c8b294d464e4156f
yiz <yizhar.gilboa@planckresolution.com>
2020-02-20 21:23:00 +0200

    added hashmap for score fast calculation

--- src/com/hashcode/ProblemSolver.java
+++ src/com/hashcode/ProblemSolver.java
@@ -1,8 +1,6 @@
 package com.hashcode;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.lang.System.currentTimeMillis;
@@ -13,20 +11,27 @@ public class ProblemSolver {
     private Map<Integer, Integer> libraryScore;
 
     public List<LibrarySubmission> solve(InputData inputData) {
+        libraryScore = new HashMap<>();
         books = inputData.books;
 
 
         inputData.libraries.forEach(lib -> libraryScore.put(lib.getLibraryIndex(), getLibraryScore(lib)));
-        inputData.libraries.sort(Comparator.comparingInt(this::getLibraryScore).reversed());
+        inputData.libraries.sort(Comparator.comparingInt(this::queryScore).reversed());
 
         long s = currentTimeMillis();
         inputData.libraries.forEach(l -> books.sort(Comparator.comparingInt(o -> books.get(o))));
         long t = currentTimeMillis();
+
         System.out.println(t - s);
 
 
         return inputData.libraries.stream().map(l -> new LibrarySubmission(l.getLibraryIndex(), l.books)).collect(Collectors.toList());
     }
+
+    private int queryScore(Library lib) {
+        return libraryScore.get(lib.getLibraryIndex());
+    }
+
 
     private int getLibraryScore(Library lib1) {
         return lib1.getBooks().stream().mapToInt(x -> books.get(x)).sum();
