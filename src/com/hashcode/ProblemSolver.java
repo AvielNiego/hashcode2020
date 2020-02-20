e16b1efd6be09a1573a7eee8b10aa7f1516ada12
yiz <yizhar.gilboa@planckresolution.com>
2020-02-20 23:05:43 +0200

    added penalty for bad days

--- src/com/hashcode/ProblemSolver.java
+++ src/com/hashcode/ProblemSolver.java
@@ -8,7 +8,6 @@ import static java.lang.System.currentTimeMillis;
 
 public class ProblemSolver {
 
-    public static final int NEXT_LIBS_WINDOW = 5;
     private List<Integer> books;
     private Map<Integer, Double> libraryScore;
     private InputData inputData;
@@ -35,6 +34,7 @@ public class ProblemSolver {
             remainingDays -= chosenLib.getSignUpTime();
         }
 
+
         return chosenLibs;
     }
 
@@ -42,27 +42,7 @@ public class ProblemSolver {
         libraries.forEach(l -> l.booksIndex.sort(Comparator.comparingInt(o -> books.get(o))));
         libraries.forEach(lib -> libraryScore.put(lib.getLibraryIndex(), getLibraryScore(lib)));
         libraries.sort(Comparator.comparingDouble(this::queryScore).reversed());
-
-        double avgScorePerDayForTopLibs = getAvgScoreForTopLibs(libraries);
-
-        libraries.forEach(lib -> libraryScore.put(lib.getLibraryIndex(), getLibraryScore(lib, avgScorePerDayForTopLibs)));
         return libraries.remove(0);
-    }
-
-    private double getAvgScoreForTopLibs(List<Library> libraries) {
-        return libraries.stream().limit(NEXT_LIBS_WINDOW).mapToDouble(this::getAvgGainForLibrary).sum() / NEXT_LIBS_WINDOW;
-    }
-
-    private int getAvgGainForLibrary(Library lib) {
-        return getBooksUpToRemainingTime(lib).mapToInt(x -> books.get(x)).sum() / getRemainingDays(lib);
-    }
-
-    private Double getLibraryScore(Library lib, double avgScorePerDayForTopLibs) {
-        Stream<Integer> booksUpToRemainingTime = getBooksUpToRemainingTime(lib);
-        int libBookSum = booksUpToRemainingTime
-                .mapToInt(x -> books.get(x))
-                .sum();
-        return libBookSum - (lib.getSignUpTime() * avgScorePerDayForTopLibs);
     }
 
     private double queryScore(Library lib) {
@@ -70,23 +50,38 @@ public class ProblemSolver {
     }
 
 
-    private double getLibraryScore(Library lib) {
-        Stream<Integer> booksUpToRemainingTime = getBooksUpToRemainingTime(lib);
+    private double getLibraryScore(Library lib1) {
+        Stream<Integer> booksUpToRemainingTime = getBooksUpToRemainingTime(lib1);
         int libBookSum = booksUpToRemainingTime
                 .mapToInt(x -> books.get(x))
                 .sum();
-        return libBookSum;
-        // - (lib.getSignUpTime() * avgScorePerDay)
+        return libBookSum - (lib1.getSignUpTime() * avgScorePerDay);
     }
 
     private Stream<Integer> getBooksUpToRemainingTime(Library lib1) {
-        long daysRemaining = getRemainingDays(lib1);
+        long daysRemaining = Math.max(inputData.getDaysForScanning() - lib1.getSignUpTime(), 0);
         return lib1.getBooksIndex().stream()
                 .filter(bookidx -> !chosenBooks.contains(bookidx))
                 .limit(daysRemaining * lib1.getShipsPerDay());
     }
 
+    private Stream<Integer> getBooksFromXtimeUpToRemainingTime(Library lib1, double fracTime) {
+        fracTime = 0.5;
+        long daysRemaining = getRemainingDays(lib1);
+        long booksWeCanTakeFromThisLib = daysRemaining * lib1.getShipsPerDay();
+        return lib1.getBooksIndex().stream()
+                .filter(bookidx -> !chosenBooks.contains(bookidx))
+                .limit(booksWeCanTakeFromThisLib)
+                .skip(Math.round(fracTime * booksWeCanTakeFromThisLib))
+                ;
+    }
+
     private int getRemainingDays(Library lib1) {
         return Math.max(inputData.getDaysForScanning() - lib1.getSignUpTime(), 0);
     }
+
+
 }
+
+
+
